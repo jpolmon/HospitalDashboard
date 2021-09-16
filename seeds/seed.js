21102ad0e83816442f5b9125844c3eea62de26c7
Sally Alvarenga <sallyalvarenga35@gmail.com>
2021-09-16 13:58:13 -0500

    cfinished making seed files and got them working

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,10 +1,9 @@
 const sequelize = require("../config/connection");
 const { Patient, Doctor, Medicine, Treatment } = require("../models");
 
-const patientData = require("./patientData.json");
-const doctorData = require("./doctorData.json");
-const medicineData = require("./medicineData.json");
-const treatmentData = require("./treatmentData.json");
+const patientData = require("./patientSeedData.json");
+const doctorData = require("./doctorSeedData.json");
+const medicineData = require("./medicationSeedData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -15,6 +14,7 @@ const seedDatabase = async () => {
   });
 
   const patients = [];
+  // reference Unit 13, activity 21
   for (const patient of patientData) {
     patients.push(
       await Patient.create({
@@ -24,24 +24,24 @@ const seedDatabase = async () => {
     );
   }
 
-  await Medicine.bulkCreate(medicineData);
+  const medications = await Medicine.bulkCreate(medicineData);
 
-  await Treatment.bulkCreate(treatmentData);
+  // await Treatment.bulkCreate(treatmentData);
 
-  //   for (let i = 0; i < 15; i++) {
-  //     const { id: ranPatientId } =
-  //       patients[Math.floor(Math.random() * patients.length)];
+  for (let i = 0; i < 15; i++) {
+    const { id: ranPatientId } =
+      patients[Math.floor(Math.random() * patients.length)];
 
-  //     const { id: ranMedicineId } =
-  //       medications[Math.floor(Math.random() * medications.length)];
+    const { id: ranMedicineId } =
+      medications[Math.floor(Math.random() * medications.length)];
 
-  //     await Treatment.create({
-  //       patient_id: ranPatientId,
-  //       medicine_id: ranMedicineId,
-  //     }).catch((err) => {
-  //       console.log(err);
-  //     });
-  //   }
+    await Treatment.create({
+      patient_id: ranPatientId,
+      medicine_id: ranMedicineId,
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
 
   process.exit(0);
 };

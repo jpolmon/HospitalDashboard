17f8639a01ed4fb99d5039434f337d9a5d32b223
Sally Alvarenga <sallyalvarenga35@gmail.com>
2021-09-19 16:58:18 -0500

    stashing minor changes before a merge

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Doctor, Patient, Treatment } = require("../models");
+const { Doctor, Patient, Medicine } = require("../models");
 const withAuth = require("../utils/auth");
 
 router.get("/", (req, res) => {
@@ -25,20 +25,21 @@ router.get("/doctorView", withAuth, async (req, res) => {
     });
     const doctor = doctorData.get({ plain: true });
 
-    // const patientData = await Patient.findAll({
-    //   include: [
-    //     {
-    //       model: Patient,
-    //       // attributes: ["firstName, lastName"],
-    //     },
-    //   ],
-    // });
-    // const patients = patientData.get({ plain: true });
-    res.json(doctor);
-    // res.render("doctorView", {
-    //   ...doctor,
-    //   logged_in: req.session.logged_in,
-    // });
+    const patientData = await Patient.findAll({
+      include: [
+        {
+          model: Patient,
+          Treatment,
+          // attributes: ["firstName, lastName"],
+        },
+      ],
+    });
+    const patients = patientData.get({ plain: true });
+    // res.json(doctor);
+    res.render("doctorView", {
+      ...doctor,
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -61,31 +62,31 @@ router.get("/patientView", async (req, res) => {
   }
 });
 
-router.get('/dashboard', withAuth, async (req, res) => {
+router.get("/dashboard", withAuth, async (req, res) => {
   try {
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
+      attributes: { exclude: ["password"] },
       include: [{ model: Patient, Medicine }],
     });
 
     const user = userData.get({ plain: true });
 
-    res.render('dashboard', {
+    res.render("dashboard", {
       ...user,
-      logged_in: true
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/dashboard');
+    res.redirect("/dashboard");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 
 // router.get('/signup', (req, res) => {
